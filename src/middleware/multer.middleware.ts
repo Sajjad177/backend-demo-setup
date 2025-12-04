0f1034fe1ed839a925cfc7e333b008e034a79e6b
Sajjad177 <sajjadhossainx06@gmail.com>
2025-12-05 02:10:38 +0600

    code updated

--- src/middleware/multer.middleware.ts
+++ src/middleware/multer.middleware.ts
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
 export const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 10 * 1024 * 1024 }, // 5MB limit
   fileFilter: (req, file, cb) => {
     const filetypes = /jpeg|jpg|pdf|png|mp4|avi|mov|avif|webp/;
 
